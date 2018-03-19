a8d72692aa20b6ad30b9902e31c9560c187a60d4
zero <1974728494@qq.com>
2018-03-19 18:02:05 +0800

    crop

--- crop.php
+++ crop.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 	$targ_w = $targ_h = 150;
 	$jpeg_quality = 90;
 
-	$src = 'demo_files/pool.jpg';
+	$src = 'images/img2.jpg';
 	$img_r = imagecreatefromjpeg($src);
 	$dst_r = ImageCreateTrueColor( $targ_w, $targ_h );
 
@@ -32,20 +32,75 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 <head>
   <title>Live Cropping Demo</title>
   <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
-  <script src="../js/jquery.min.js"></script>
-  <script src="../js/jquery.Jcrop.js"></script>
-  <link rel="stylesheet" href="demo_files/main.css" type="text/css" />
-  <link rel="stylesheet" href="demo_files/demos.css" type="text/css" />
-  <link rel="stylesheet" href="../css/jquery.Jcrop.css" type="text/css" />
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery.Jcrop.min.js"></script>
+  <link rel="stylesheet" href="css/Jcrop-main.css" type="text/css" />
+  <link rel="stylesheet" href="css/Jcrop-demos.css" type="text/css" />
+  <link rel="stylesheet" href="css/jquery.Jcrop.min.css" type="text/css" />
 
 <script type="text/javascript">
 
+   // function getFileUrl(sourceId) {   
+   //      var url;   
+   //      if (navigator.userAgent.indexOf("MSIE")>=1) { // IE   
+   //      url = document.getElementById(sourceId).value;   
+   //  }   
+   //      else if(navigator.userAgent.indexOf("Firefox")>0) { // Firefox   
+   //      url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));   
+   //  }   
+   //      else if(navigator.userAgent.indexOf("Chrome")>0) { // Chrome   
+   //      url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));   
+   //  }   
+   //      return url;   
+   //  }  
+   //  function preImg(sourceId, targetId) {   
+   //      var url = getFileUrl(sourceId);   
+   //      var imgPre = document.getElementById(targetId);   
+   //      imgPre.src = url;   
+   //  }   
+
   $(function(){
+      // Create variables (in this scope) to hold the API and image size
+            var jcrop_api,
+                boundx,
+                boundy,
+
+                // Grab some information about the preview pane
+                $preview = $('#preview-pane'),
+                $pcnt = $('#preview-pane .preview-container'),
+                $pimg = $('#preview-pane .preview-container img'),
+
+                xsize = $pcnt.width(),
+                ysize = $pcnt.height();
 
     $('#cropbox').Jcrop({
-      aspectRatio: 1,
-      onSelect: updateCoords
+                onChange: updatePreview,
+                onSelect: updateCoords,
+                aspectRatio: xsize / ysize
+            }, function() {
+                // Use the API to get the real image size
+                var bounds = this.getBounds();
+                boundx = bounds[0];
+                boundy = bounds[1];
+                // Store the API in the jcrop_api variable
+                jcrop_api = this;
+
+                // Move the preview into the jcrop container for css positioning
+                $preview.appendTo(jcrop_api.ui.holder);
     });
+     function updatePreview(c) {
+                if (parseInt(c.w) > 0) {
+                    var rx = xsize / c.w;
+                    var ry = ysize / c.h;
+
+                    $pimg.css({
+                        width: Math.round(rx * boundx) + 'px',
+                        height: Math.round(ry * boundy) + 'px',
+                        marginLeft: '-' + Math.round(rx * c.x) + 'px',
+                        marginTop: '-' + Math.round(ry * c.y) + 'px'
+                    });
+                }
+            };
 
   });
 
@@ -73,7 +128,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     font-size: 24px;
     display: block;
   }
-
+ .jcrop-holder #preview-pane {        
+    display: block;        
+    position: absolute;
+    z-index: 2000;
+    top: 10px;
+    right: -280px;
+    padding: 6px;
+    border: 1px rgba(0, 0, 0, .4) solid;
+    background-color: white;
+    -webkit-border-radius: 6px;
+    -moz-border-radius: 6px;
+    border-radius: 6px;
+    -webkit-box-shadow: 1px 1px 5px 2px rgba(0, 0, 0, 0.2);
+    -moz-box-shadow: 1px 1px 5px 2px rgba(0, 0, 0, 0.2);
+    box-shadow: 1px 1px 5px 2px rgba(0, 0, 0, 0.2);       
+ }
+        /* The Javascript code will set the aspect ratio of the crop
+   area based on the size of the thumbnail preview,
+   specified here */
+        
+        #preview-pane .preview-container {
+            width: 150px;
+            height: 150px;
+            overflow: hidden;
+        }
 
 </style>
 
@@ -84,34 +163,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 <div class="row">
 <div class="span12">
 <div class="jc-demo-box">
-
-<div class="page-header">
-<ul class="breadcrumb first">
-  <li><a href="../index.html">Jcrop</a> <span class="divider">/</span></li>
-  <li><a href="../index.html">Demos</a> <span class="divider">/</span></li>
-  <li class="active">Live Demo (Requires PHP)</li>
-</ul>
-<h1>Server-based Cropping Behavior</h1>
-</div>
-
 		<!-- This is the image we're attaching Jcrop to -->
-		<img src="demo_files/pool.jpg" id="cropbox" />
+      <!-- <input type="file" name="imgOne" id="imgOne1" onchange="preImg(this.id,'imgPre');" />    -->
+    <!-- <img id="imgPre" src="" width="300px" height="400px" style="display: block;" />    -->
+		<img src="images/img2.jpg"  id="cropbox" />
+     <div id="preview-pane">
+           <div class="preview-container">
+                <img src="images/img2.jpg" class="jcrop-preview" alt="Preview" />
+           </div>
+     </div>
+
 
 		<!-- This is the form that our event handler fills -->
 		<form action="crop.php" method="post" onsubmit="return checkCoords();">
-			<input type="hidden" id="x" name="x" />
-			<input type="hidden" id="y" name="y" />
-			<input type="hidden" id="w" name="w" />
-			<input type="hidden" id="h" name="h" />
-			<input type="submit" value="Crop Image" class="btn btn-large btn-inverse" />
+			  <input type="hidden" id="x" name="x" />
+			  <input type="hidden" id="y" name="y" />
+			  <input type="hidden" id="w" name="w" />
+			  <input type="hidden" id="h" name="h" />
+			  <input type="submit" value="Crop Image" class="btn btn-large btn-inverse" />
 		</form>
 
-		<p>
-			<b>An example server-side crop script.</b> Hidden form values
-			are set when a selection is made. If you press the <i>Crop Image</i>
-			button, the form will be submitted and a 150x150 thumbnail will be
-			dumped to the browser. Try it!
-		</p>
+	
 
 
 	</div>
